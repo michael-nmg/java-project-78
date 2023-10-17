f2bf879c9045c3feb65fc6be1a16fc4bce0c7d92
michael-nmg <michael.nmg@gmail.com>
2023-10-17 03:11:52 +0300

    add NumberSchema class

---
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -0,0 +1,30 @@
+package hexlet.code.schemas;
+
+import java.util.function.Predicate;
+
+import hexlet.code.requirements.UseRequirement;
+
+public final class NumberSchema extends BaseSchema {
+
+    public NumberSchema() {
+        addPredicate(Integer.class::isInstance);
+    }
+
+    public NumberSchema required() {
+        setState(new UseRequirement());
+        return this;
+    }
+
+    public NumberSchema positive() {
+        Predicate<Object> predicate = object -> Integer.signum(((Integer) object)) > 0;
+        this.addPredicate(predicate);
+        return this;
+    }
+
+    public NumberSchema range(int left, int right) {
+        Predicate<Object> predicate = object -> left <= ((Integer) object) && ((Integer) object) <= right;
+        this.addPredicate(predicate);
+        return this;
+    }
+
+}
