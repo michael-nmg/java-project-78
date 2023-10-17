114c3954ae4aef50b466fd5ae6a9cbfc95757089
michael-nmg <michael.nmg@gmail.com>
2023-10-17 03:12:33 +0300

    fix Validator class test

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
+import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,6 +21,13 @@ class ValidatorTest {
     void stringSchemaTest() {
         var actual = validator.string().getClass();
         var expected = StringSchema.class;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void numberSchemaTest() {
+        var actual = validator.number().getClass();
+        var expected = NumberSchema.class;
         assertEquals(expected, actual);
     }
 
