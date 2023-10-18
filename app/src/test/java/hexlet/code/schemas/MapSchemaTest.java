8e60046a2710b8fd6f3ebfcbacbce748986e328f
michael-nmg <michael.nmg@gmail.com>
2023-10-18 22:17:45 +0300

    fix MapSchemaTest

--- app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
@@ -15,7 +15,7 @@ class MapSchemaTest {
 
     private MapSchema schema;
     private Validator validator;
-    private Map<String, String> data = new HashMap<>();
+    private final Map<String, String> data = new HashMap<>();
 
     @BeforeEach
     void init() {
@@ -28,7 +28,7 @@ class MapSchemaTest {
     void nonRequiredTest() {
         assertTrue(schema.isValid(null));
         assertTrue(schema.sizeof(0).isValid(null));
-        assertTrue(schema.sizeof(0).isValid(5));
+        assertFalse(schema.sizeof(0).isValid(5));
     }
 
     @Test
@@ -48,6 +48,34 @@ class MapSchemaTest {
 
         data.put("key2", "value2");
         assertTrue(schema.isValid(data));
+    }
+
+    @Test
+    void shapeTest() {
+        Map<String, BaseSchema> schemas = new HashMap<>();
+        schemas.put("name", validator.string().required());
+        schemas.put("age", validator.number().positive());
+        schema.shape(schemas);
+
+        Map<String, Object> human1 = new HashMap<>();
+        human1.put("name", "Kolya");
+        human1.put("age", 100);
+        assertTrue(schema.isValid(human1));
+
+        Map<String, Object> human2 = new HashMap<>();
+        human2.put("name", "Maya");
+        human2.put("age", null);
+        assertTrue(schema.isValid(human2));
+
+        Map<String, Object> human3 = new HashMap<>();
+        human3.put("name", "");
+        human3.put("age", null);
+        assertFalse(schema.isValid(human3));
+
+        Map<String, Object> human4 = new HashMap<>();
+        human4.put("name", "Valya");
+        human4.put("age", -5);
+        assertFalse(schema.isValid(human4));
     }
 
     @Test
